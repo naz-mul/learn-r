298ce7a855ed8a3e8d003686b3da86508a8487d9
Nazmul Alam <Nazmul.Alam@students.ittralee.ie>
2017-03-16 19:52:19 +0000

    basics of R

---
+++ Basic_R.R
@@ -0,0 +1,63 @@
+x <- 'Hello World'
+print(x) # print by function
+x # print by value
+
+# create a vector
+v <- c(1, 2, 3)
+
+# create a sequence
+s <- 1:5
+
+# create a matrix
+m <- matrix(data = 1:6, nrow = 2, ncol = 3)
+
+# create an array
+a <- array(data = 1:8, dim = c(2, 2, 2))
+
+# creating a list
+l <- list(TRUE, 123L, 2.34, "abc")
+
+
+# creating a factor
+categories <- c("Male", "Female", "Male", "Male", "Female")
+factor <- factor(categories)
+levels(factor) # see a list of unique names
+unclass(factor) # see the underlying integer 
+
+# creating a dataframe
+df <- data.frame(Name = c("Cat", "Dog", "Cow", "Pig"), 
+                 HowMany = c(5, 10, 15, 20), 
+                 IsPet = c(TRUE, TRUE, FALSE, FALSE))
+
+
+# Index data frame by row and column
+df[1, 2]
+
+# Index data frame by column
+df[, 2]
+df["HowMany"]
+df$HowMany 
+
+# Index data frames by row
+df[1, ]
+
+
+# Subsetting data frames
+# allows to slice and dice data in very flexible ways
+df[c(2, 4), ] # return 2 and 4 rows
+df[2:4, ] # return 2 through 4 rows
+df[c(TRUE, FALSE, TRUE, FALSE), ] # return by logical values
+df[df$IsPet == TRUE, ] # return rows where pet is true
+df[df$HowMany > 10, ] # return rows where pet is greater than
+df[df$Name %in% c("Cat", "Cow"), ] # return rows where name matches
+
+# R is a vectorised language
+# In R arguements can be passed by name or in order
+
+# Named vs ordered arguments
+m <- matrix(data = 1:6, nrow = 2, ncol = 3) # create a matrix m in named fashion
+n <- matrix(1:6, 2, 3) # create a matrix n in ordered fashion
+m == n # are the values of m and n are equal
+identical(m, n) # find if m and n are identical
+
+
---
+++ DataScienceWith.R
@@ -0,0 +1,33 @@
+# ETL
+# Extract, Transform and Load
+# Import, Clean, Transform, Export
+
+# IMPORTING DATA
+# File based, web based, database, statisical etc.
+
+# CLEANING DATA
+# Most raw data are not suitable for using
+
+# Process of cleaning data includes
+## Reshaping data,
+## renaming columns, 
+## convert data types, 
+## ensure proper encoding,
+## ensure internal consistency,
+## handle errors and outliers
+
+# Libraries to use for cleaning
+# base, tidyr, reshape2, stringr, lubridate, validate
+
+# TRANSFORING DATA
+## Select columns, filter rows, groups rows, order rows, merge tables
+
+# Librarie to use for transforming data
+# base, plyr, dplyr, data.table, sqldf
+
+# EXPORTING DATA
+# file based, web based, databases, statistical etc.
+
+# USING DPLYR
+# Tools include: select, filter, mutate, summarise, arrange
+
