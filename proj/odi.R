9b2007f8a8d0e94ab3e5f1d73896f5ffd1afa6c5
Nazmul Alam <Nazmul.Alam@students.ittralee.ie>
2017-04-19 22:15:01 +0100

    data is merged - exported to csv

--- proj/odi.R
+++ proj/odi.R
@@ -5,9 +5,6 @@
 
 setwd("~/Desktop/learn-r/proj")
 
-this.path <- "odi_csv_male"
-this.pattern <- ".csv$"
-
 import.mult.csv <- function(path, pattern, ...) {
   tmp.files.list <- list.files(path, pattern, full.names = TRUE)
   tmp.list.data <- list(length = length(tmp.files.list))
@@ -20,7 +17,34 @@ import.mult.csv <- function(path, pattern, ...) {
   tmp.list.data
 }
 
+sbind <- function(x, y, fill=NA) {
+  sbind.fill <- function(d, cols){ 
+    for(c in cols)
+      d[[c]] <- fill
+    d
+  }
+  
+  x <- sbind.fill(x, setdiff(names(y),names(x)))
+  y <- sbind.fill(y, setdiff(names(x),names(y)))
+  
+  rbind(x, y)
+}
+
+bind_data <- function(data.t) {
+  for(i in 2:length(data)) {
+    tmp.df <- as.data.frame(data[i])
+    tmp.df.t <- as.data.frame(t(tmp.df))
+    row.names(tmp.df.t) <- NULL
+    data.t <- sbind(data.t, tmp.df.t, fill = TRUE)
+  }
+  data.t
+}
+
+
 # Import all the csv files as list
+this.path <- "odi_csv_male"
+this.pattern <- ".csv$"
+
 data <-
   import.mult.csv(
     this.path,
@@ -29,42 +53,46 @@ data <-
     skip = 1,
     row.names = NULL,
     header = F,
-    colClasses = c("NULL", NA, NA)
+    colClasses = c("NULL", "NULL", NA)
   )
 
-# Convert list to data frame
-df <- data.frame(lapply(data.frame(t(sapply(
-  data, `[`
-))), unlist))
-
-# Remove row names
-row.names(df) <- NULL
+# Convert to dataframe
+library(plyr)
+cric.data <- as.data.frame(data[1])
+cric.data.t <- as.data.frame(t(cric.data))
+row.names(cric.data.t) <- NULL
 
 
-# create a data frame with first 19 rows
-cric.data <- df[1:19, ]
-
-# extract first 19 rows from column one to set it as header
-names <- df$V2[1:19]
-names.char <- as.character(names)
-names.char
-
-# transpose rows as columns
-cric.data <- as.data.frame(t(cric.data))
-cric.data <- cric.data[-1, ]
-row.names(cric.data) <- NULL
-names(cric.data) <- names.char
-
-new.cric.data <- cric.data
+# Extract column names
+names.data <-
+  import.mult.csv(
+    this.path,
+    this.pattern,
+    nrows = 19,
+    skip = 1,
+    row.names = NULL,
+    header = F,
+    colClasses = c("NULL", NA, "NULL")
+  )
 
 library(plyr)
-for(i in seq(from = 20, to=nrow(df), by=19)) {
-  from <- i
-  to <- from + 18
-  tmp.cric.data <- df[from:to, ]
-  tmp.cric.data <- as.data.frame(t(tmp.cric.data))
-  tmp.cric.data <- tmp.cric.data[-1, ]
-  row.names(tmp.cric.data) <- NULL
-  names(tmp.cric.data) <- names.char
-  cric.data <- rbind(cric.data, tmp.cric.data)
-}
+names <- as.data.frame(names.data[1])
+names.char <- as.character(names$V2)
+
+
+# Bind list of dataframes to a single dataframe
+# First create a single data-frame
+library(plyr)
+cric.data <- as.data.frame(data[1])
+cric.data.t <- as.data.frame(t(cric.data))
+row.names(cric.data.t) <- NULL
+
+# Then, merge all the other data frames to the single data frame
+df <- bind_data(cric.data.t)
+
+
+# Set column names
+names(df) <- names.char
+
+# Get a subset of the data - remove last 3 invalid columns
+df.sub <- df[c(-20,-21, -22)]
